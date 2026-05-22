25722ffafd7551931000f6a76475c29aa8b94180
Nick <HypeBass.LifeBalance@gmail.com>
2026-05-21 23:36:43 -0300

    feat: added the logic for the hold btn, new game and the automatic switch between players

--- script.js
+++ script.js
@@ -6,43 +6,88 @@ const scorePlayerTwoEl = document.getElementById("score--1");
 const currentPlayerOneEl = document.getElementById("current--0");
 const currentPlayerTwoEl = document.getElementById("current--1");
 
-scorePlayerOneEl.textContent = 0;
-scorePlayerTwoEl.textContent = 0;
-
 // Variables for the players
-const player = document.querySelector(".player");
-const playerOne = document.querySelector(".player--0");
-const playerTwo = document.querySelector(".player--1");
-const Winner = document.querySelector(".player--winner");
+const playerOneEl = document.querySelector(".player--0");
+const playerTwoEl = document.querySelector(".player--1");
 
 // Variables for the buttons
-const btnNewGame = document.querySelector(".btn--new");
-const btnHoldScore = document.querySelector(".btn--hold");
-const btnRollDice = document.querySelector(".btn--roll");
+const btnNewGameEl = document.querySelector(".btn--new");
+const btnHoldScoreEl = document.querySelector(".btn--hold");
+const btnRollDiceEl = document.querySelector(".btn--roll");
 
 // Dice
 const diceEl = document.querySelector(".dice");
-let dice;
 
 diceEl.classList.add("hidden");
+let dice;
 
-// Buttons
-btnRollDice.addEventListener("click", () => {
+const scores = [0, 0];
+let activePlayer = 0;
+let currentScore = 0;
+
+// Switch player
+const switchPlayer = function () {
+	document.getElementById(`current--${activePlayer}`).textContent = 0;
+	currentScore = 0;
+	activePlayer = activePlayer === 0 ? 1 : 0;
+	playerOneEl.classList.toggle("player--active");
+	playerTwoEl.classList.toggle("player--active");
+	return;
+};
+
+// Save score to current player
+const saveScore = function () {
+	scores[activePlayer] += currentScore;
+	document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+	if (scores[activePlayer] > 10) {
+		document.querySelector(`.player--${activePlayer}`).classList.add("player--winner");
+		document.querySelector(`.player--${activePlayer}`).classList.remove("player--active");
+		btnHoldScoreEl.classList.add("hidden");
+		btnRollDiceEl.classList.add("hidden");
+		return;
+	}
+
+	switchPlayer();
+	return;
+};
+
+// ____Buttons____
+
+// Roll dice and display the score
+btnRollDiceEl.addEventListener("click", () => {
 	dice = Math.trunc(Math.random() * 6) + 1;
 	diceEl.classList.remove("hidden");
 	diceEl.src = `./img/dice-${dice}.png`;
+
+	if (dice !== 1) {
+		currentScore += dice;
+		document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+		return;
+	} else {
+		switchPlayer();
+		return;
+	}
 	return;
 });
-btnHoldScore.addEventListener("click", () => {
-	if (player.classList.contains("player--0") && player.classList.contains("player--active")) {
-		scorePlayerOne = currentPlayerOne;
-		console.log(scorePlayerOne);
-	}
-	if (player.classList.contains("player--1") && player.classList.contains("player--active")) {
-		scorePlayerTwo = currentPlayerTwo;
-	}
+
+// Hold score
+btnHoldScoreEl.addEventListener("click", () => {
+	saveScore();
 });
 
-console.log(scorePlayerTwo);
-console.log(currentPlayerOne);
-console.log(currentPlayerTwo);
+// New game
+btnNewGameEl.addEventListener("click", () => {
+	document.querySelector(`.player--${activePlayer}`).classList.remove("player--winner");
+	diceEl.classList.add("hidden");
+	playerOneEl.classList.add("player--active");
+	playerTwoEl.classList.remove("player--active");
+	btnHoldScoreEl.classList.remove("hidden");
+	btnRollDiceEl.classList.remove("hidden");
+	activePlayer = 0;
+	currentScore = 0;
+	scorePlayerOneEl.textContent = 0;
+	scorePlayerTwoEl.textContent = 0;
+	currentPlayerOneEl.textContent = 0;
+	currentPlayerTwoEl.textContent = 0;
+});
